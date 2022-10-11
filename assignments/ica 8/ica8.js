888d1af946aaff5d0a9152e649db7cac02ad0de3
KrischanKramer <112125623+KrischanKramer@users.noreply.github.com>
2022-10-11 15:54:43 -0600

    ica8

---
+++ assignments/ica 8/ica8.js
@@ -0,0 +1,27 @@
+// let body = document.querySelector('body');
+// body.addEventListener('click', giveAlert);
+
+// function giveAlert(){
+//     alert('this is an alert');
+// }
+
+
+let body = document.querySelector('body');
+body.addEventListener('click', switchBackgroundColor);
+
+function switchBackgroundColor(){
+    ('this is an alert');
+}
+
+function red(){ document.body.style.background = "red"; }
+function white(){ document.body.style.background = "white"; }
+
+let h1 = document.querySelector('h1');
+h1.addEventListener('click', changeFont);
+
+function changeFont(){
+    h1.style.fontSize = '100px'
+}
+
+
+
