094f8029f35787fb0a8d731e3aa24001a0a99e24
KrischanKramer <112125623+KrischanKramer@users.noreply.github.com>
2022-11-10 18:01:43 -0700

    assignment names fixed

---
+++ assignments/assigment_12take2/ass12.js
@@ -0,0 +1,33 @@
+var randomkey;
+var numberEnter;
+
+window.onload = function() {
+    randomNumber();
+}
+
+window.setInterval(function(){randomNumber();}, 1000);
+
+function randomNumber() {
+  var label = document.getElementById("out");
+  randomkey = 97 + Math.floor(Math.random() * 26);
+  numbeEnter = Math.floor(Math.random() * 10);
+  label.innerHTML = "Please press " + String.fromCharCode(randomkey) + " to enter " + numberEnter;
+}
+
+function checkKeyPress(e){
+  var keynum;
+  if(window.event) {keynum = e.keyCode;}
+  else if(e.which){keynum = e.which;}
+  if (keynum == randomkey) {
+    var current = document.getElementById("phone").innerHTML;
+    document.getElementById("phone").innerHTML = current + numberEnter;
+  }
+  else {
+    alert("You have pressed the incorrect key. Please Try again")
+  }
+}
+
+function submitNumber() {
+    alert("ERROR PLEASE TRY AGAIN");
+    document.getElementById("phone").innerHTML = "";
+}
